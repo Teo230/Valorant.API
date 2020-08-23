eeb2c52e750503f5b7a85f127729b6133839d77e
Matteo Fantin <matteo.fantin@kodis.com>
2020-08-23 19:24:16 +0200

    fix idListDTO

--- Valorant.Rest.API/ModelDTO/IdListDTO.cs
+++ Valorant.Rest.API/ModelDTO/IdListDTO.cs
@@ -8,22 +8,23 @@ namespace Valorant.Rest.API.ModelDTO
 {
     public class IdListDTO
     {
-        public Content Characters { get; set; }
-        public Content Maps { get; set; }
-        public Content Chromas { get; set; }
-        public Content Skins { get; set; }
-        public Content SkinLevels { get; set; }
-        public Content Attachments { get; set; }
-        public Content Equips { get; set; }
-        public Content Themes { get; set; }
-        public Content GameModes { get; set; }
-        public Content Sprays { get; set; }
-        public Content SprayLevels { get; set; }
-        public Content Charms { get; set; }
-        public Content CharmLevels { get; set; }
-        public Content PlayerCards { get; set; }
-        public Content PlayerTitles { get; set; }
-        public Content StorefrontItems { get; set; }
+        public List<Content> Characters { get; set; }
+        public List<Content> Maps { get; set; }
+        public List<Content> Chromas { get; set; }
+        public List<Content> Skins { get; set; }
+        public List<Content> SkinLevels { get; set; }
+        public List<Content> Attachments { get; set; }
+        public List<Content> Equips { get; set; }
+        public List<Content> Themes { get; set; }
+        public List<Content> GameModes { get; set; }
+        public List<Content> Sprays { get; set; }
+        public List<Content> SprayLevels { get; set; }
+        public List<Content> Charms { get; set; }
+        public List<Content> CharmLevels { get; set; }
+        public List<Content> PlayerCards { get; set; }
+        public List<Content> PlayerTitles { get; set; }
+        public List<Content> StorefrontItems { get; set; }
+        public List<Season> Seasons { get; set; }
 
         public class Content
         {
@@ -32,6 +33,18 @@ namespace Valorant.Rest.API.ModelDTO
             public string AssetName { get; set; }
             public string AssetPath { get; set; }
             public bool IsEnabled { get; set; }
+        }
+
+        public class Season
+        {
+            public string ID { get; set; }
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public DateTime StartTime { get; set; }
+            public DateTime EndTime { get; set; }
+            public bool IsEnabled { get; set; }
+            public bool IsActive { get; set; }
+            public bool DevelopmentOnly { get; set; }
         }
     }
 }
